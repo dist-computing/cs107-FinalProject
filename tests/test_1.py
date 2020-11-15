0388c2fa065a8ad2846cb15aa7aeb050c2eb1a37
Matt <fernandes@alumni.harvard.edu>
2020-11-15 20:39:27 +0000

    making travis work

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,14 +1,15 @@
 import sys
-sys.path.insert(1, '../AAD')
+sys.path.insert(1, '../AAD/')
 
 import numpy
-from AAD import AADVariable as AD
+# from AAD import AADVariable as AD
+import AAD as AD
 import math
 
 
 def test_sin():
-    x = AD(3.14159265358/2)
-    print(sin(x))
+    x = AD.AADVariable(3.14159265358/2)
+    print(AD.sin(x))
     print(3*x + 5)
 test_sin()
 
