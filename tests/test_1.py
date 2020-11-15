1688b25ec2c62cf95f719d3f9878d8afbc775c35
Matt <fernandes@alumni.harvard.edu>
2020-11-15 20:25:07 +0000

    making travis work

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,9 +1,9 @@
 import pytest
-import AAD
+from AAD import AADVariable as AD
 import math
 
 
 def test_1():
-    x = AADVariable(math.pi/2)
+    x = AD(math.pi/2)
     print(sin(x))
     print(3*x + 5)
