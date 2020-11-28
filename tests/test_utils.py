32993b278a3b5cb82dc476b6f096c038f4442d95
Haipeng <jimmielin@gmail.com>
2020-11-28 04:19:49 +0000

    Fix tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,14 +2,14 @@ import sys
 sys.path.insert(1, '../AAD/')
 import numpy as np
 import AAD as AD
-import AADUtils
+import AADUtils as ADU
 import math
 
 def test_align_lists():
-    t1 = AADUtils.align_lists([1, 0, 1, 2], [0, 0, 5, 0]) # ([1, 0, 1, 2], [0, 0, 5, 0])
-    t2 = AADUtils.align_lists([1], [0, 0, 5, 0]) # ([1, 0, 0, 0], [0, 0, 5, 0])
-    t3 = AADUtils.align_lists([1, 3], [2]) # ([1, 3], [2, 0])
-    t4 = AADUtils.align_lists([1, 3], 9) # ([1, 3], [0, 0])
+    t1 = ADU.align_lists([1, 0, 1, 2], [0, 0, 5, 0]) # ([1, 0, 1, 2], [0, 0, 5, 0])
+    t2 = ADU.align_lists([1], [0, 0, 5, 0]) # ([1, 0, 0, 0], [0, 0, 5, 0])
+    t3 = ADU.align_lists([1, 3], [2]) # ([1, 3], [2, 0])
+    t4 = ADU.align_lists([1, 3], 9) # ([1, 3], [0, 0])
 
     assert(t1 == ([1, 0, 1, 2], [0, 0, 5, 0]))
     assert(t2 == ([1, 0, 0, 0], [0, 0, 5, 0]))
