760bb72fb7eb049ad6c2184e1b46cc86361c8098
Matt <fernandes@alumni.harvard.edu>
2020-12-06 00:33:58 +0000

    added comments to gradient descent code and fixed test bugs

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,14 +5,16 @@ import numpy as np
 import AAD as AD
 import math
 
+tol=1e-14
+
 #testing sin(x)
 def test_1():
     x = AD.AADVariable((math.pi/2))
     x = AD.sin(x)
     #value check
-    assert abs(x.val - math.sin(math.pi/2)) <  1e-15
+    assert abs(x.val - math.sin(math.pi/2)) <  tol
     #derivative check
-    assert abs(x.der - math.cos(math.pi/2)) <  1e-15
+    assert abs(x.der - math.cos(math.pi/2)) <  tol
 test_1()
 
 #testing sin(cos(x))
@@ -20,9 +22,9 @@ def test_2():
     x = AD.AADVariable((math.pi))
     x = AD.sin(AD.cos(x))
     #value check
-    assert abs(x.val - math.sin(math.cos(math.pi))) <  1e-15
+    assert abs(x.val - math.sin(math.cos(math.pi))) <  tol
     #derivative check
-    assert abs(x.der - (-math.cos(math.cos(math.pi))*math.sin(math.pi))) <  1e-15
+    assert abs(x.der - (-math.cos(math.cos(math.pi))*math.sin(math.pi))) <  tol
 test_2()
 
 #testing exp(tan(x))
@@ -30,9 +32,9 @@ def test_3():
     x = AD.AADVariable((math.pi))
     x = AD.exp(AD.tan(x))
     #value check
-    assert abs(x.val - math.exp(math.tan(math.pi))) <  1e-15
+    assert abs(x.val - math.exp(math.tan(math.pi))) <  tol
     #derivative check
-    assert abs(x.der - math.exp(math.tan(math.pi))*1/math.cos(math.pi)**2) < 1e-15
+    assert abs(x.der - math.exp(math.tan(math.pi))*1/math.cos(math.pi)**2) < tol
 test_3()
 
 #testing sinh(x) + cosh(x) + tanh(x)
@@ -40,9 +42,9 @@ def test_4():
     x = AD.AADVariable(3)
     x = AD.sinh(x) + AD.cosh(x) + AD.tanh(x)
     #value check
-    assert abs(x.val - (math.sinh(3) + math.cosh(3) + math.tanh(3))) < 1e-15
+    assert abs(x.val - (math.sinh(3) + math.cosh(3) + math.tanh(3))) < tol
     #derivative check
-    assert abs(x.der - (math.cosh(3) + 1/(math.cosh(3)**2) + math.sinh(3))) < 1e-14
+    assert abs(x.der - (math.cosh(3) + 1/(math.cosh(3)**2) + math.sinh(3))) < tol
 test_4()
 
 #log(x) - arcsin(x)
@@ -50,9 +52,9 @@ def test_5():
     x = AD.AADVariable(.5)
     x = AD.log(x) - AD.arcsin(x)
     #value check
-    assert abs(x.val - (np.log(.5) - math.asin(.5))) <  1e-15
+    assert abs(x.val - (np.log(.5) - math.asin(.5))) <  tol
     #derivative check
-    assert abs(x.der - (1/.5 - 1/math.sqrt(1 - .5**2))) <  1e-15
+    assert abs(x.der - (1/.5 - 1/math.sqrt(1 - .5**2))) <  tol
 test_5()
 
 #sqrt(x)*arctan(x) + arccos(x)
@@ -61,9 +63,9 @@ def test_6():
     x = AD.AADVariable((v))
     x = AD.sqrt(x) * AD.arctan(x) + AD.arccos(x)
     #value check
-    assert abs(x.val - (math.sqrt(v)*math.atan(v) + math.acos(v))) <  1e-15
+    assert abs(x.val - (math.sqrt(v)*math.atan(v) + math.acos(v))) <  tol
     #derivative check
-    assert abs(x.der- (math.sqrt(v)/(v**2 + 1.) - 1./(math.sqrt(1. - v**2)) + (math.atan(v)/(2. * math.sqrt(v))))) < 1e-15  
+    assert abs(x.der- (math.sqrt(v)/(v**2 + 1.) - 1./(math.sqrt(1. - v**2)) + (math.atan(v)/(2. * math.sqrt(v))))) < tol  
 test_6()
 
 #x**2/sqrt(x)
@@ -72,9 +74,9 @@ def test_7():
     x = AD.AADVariable(v)
     x = x**2/AD.sqrt(x)
     #value check
-    assert abs(x.val - v**2/math.sqrt(v)) <  1e-15
+    assert abs(x.val - v**2/math.sqrt(v)) <  tol
     #derivative check
-    assert abs(x.der-(3.*math.sqrt(v))/2. ) < 1e-15 
+    assert abs(x.der-(3.*math.sqrt(v))/2. ) < tol 
 test_7()
 
 #testing 2*cos(x)
@@ -82,9 +84,9 @@ def test_8():
     x = AD.AADVariable((math.pi))
     x = 2*AD.cos(x)
     #value check
-    assert abs(x.val - 2*math.cos(math.pi)) < 1e-15
+    assert abs(x.val - 2*math.cos(math.pi)) < tol
     #derivative check
-    assert abs(x.der + 2*math.sin(math.pi)) < 1e-15
+    assert abs(x.der + 2*math.sin(math.pi)) < tol
 test_8()
 
 #testing 2/cos(x)
@@ -92,9 +94,9 @@ def test_9():
     x = AD.AADVariable((math.pi))
     x = 2/AD.cos(x)
     #value check
-    assert abs(x.val - 2/math.cos(math.pi)) < 1e-15
+    assert abs(x.val - 2/math.cos(math.pi)) < tol
     #derivative check
-    assert abs(x.der + 2*math.tan(math.pi)/math.cos(math.pi)) < 1e-15
+    assert abs(x.der + 2*math.tan(math.pi)/math.cos(math.pi)) < tol
 test_9()
 
 #testing 2^exp(x)
@@ -102,9 +104,9 @@ def test_10():
     x = AD.AADVariable((math.pi))
     x = 2**AD.exp(x)
     #value check
-    assert abs(x.val - 2**math.exp(math.pi)) < 1e-15
+    assert abs(x.val - 2**math.exp(math.pi)) < tol
     #derivative check
-    assert abs(x.der - 2**(math.exp(math.pi))*math.exp(math.pi)*math.log(2)) < 1e-15
+    assert abs(x.der - 2**(math.exp(math.pi))*math.exp(math.pi)*math.log(2)) < tol
 test_10()
 
 def mul_edgecase():
@@ -156,7 +158,7 @@ sub_edgecase()
 def jacobian_test():
     x = AD.AADVariable((math.pi))
     x = 2**AD.exp(x)
-    assert abs(x.jacobian() - 2**(math.exp(math.pi))*math.exp(math.pi)*math.log(2)) < 1e-15
+    assert abs(x.jacobian() - 2**(math.exp(math.pi))*math.exp(math.pi)*math.log(2)) < tol
 jacobian_test()
 
 #testing repr of 2^exp(x)
