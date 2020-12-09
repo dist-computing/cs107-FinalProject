0cf46c23af363fb8baef3db393c05b1693d9d2de
Matt <fernandes@alumni.harvard.edu>
2020-12-09 01:04:31 +0000

    added bash script for updating the webpage

---
+++ UpdateIndexFromDocomentation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#copy files from main repo
+cp ../cs107-FinalProject/docs/documentation.ipynb ./documentation.ipynb
+
+#convert notebook to markdown
+jupyter nbconvert $(pwd)/documentation.ipynb --to markdown --output $(pwd)/index.md
+
+#add title to file
+sed -i '1s/^/---\n/' index.md
+sed -i '1s/^/ title: About the code \n/' index.md
+sed -i '1s/^/---\n/' index.md
+
+#commit, push and publish
+git add index.md documentation.ipynb
+git commit -m 'updated index.md with documentation <automated>'
+git push
